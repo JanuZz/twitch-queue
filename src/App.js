fca9cf47ef1e4f2e934dcfeee0dff7f2dff74efe
JanuZz <jlp02.dk@gmail.com>
2021-08-17 21:43:34 +0200

    first deploy

--- src/App.js
+++ src/App.js
@@ -27,11 +27,28 @@ function App() {
         <p className="text-center fs-3 text-white">Queue</p>
       </div>
       {people.map((person, i) => {
-        console.log(person.mod);
+        let outlineColor = "Black";
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+          person.color
+        );
+        if (result) {
+          var r = parseInt(result[1], 16);
+          var g = parseInt(result[2], 16);
+          var b = parseInt(result[3], 16);
+
+          if (r + g + b < (255 * 3) / 2) {
+            outlineColor = "White";
+          }
+        }
+
         return (
           <div className="d-flex flex-row justify-content-center align-items-center">
             <p
-              className="text-center fs-2 font-weight-bold personName"
+              className={
+                "text-center fs-2 font-weight-bold personName outline" +
+                outlineColor
+              }
               style={{
                 color: person.color,
               }}
