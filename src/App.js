960ba73a2e616cf199bee34daec43380387a24f4
JanuZz <jlp02.dk@gmail.com>
2021-08-21 14:39:13 +0200

    first deploy

--- src/App.js
+++ src/App.js
@@ -31,35 +31,9 @@ function App() {
         </p>
       </div>
       {people.map((person, i) => {
-        let outlineColor = "White";
-
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
-          person.color
-        );
-        if (result) {
-          var r = parseInt(result[1], 16);
-          var g = parseInt(result[2], 16);
-          var b = parseInt(result[3], 16);
-
-          console.log(r + g + b);
-
-          if (r + g + b > (255 * 3) / 2) {
-            outlineColor = "Black";
-          }
-        }
-
         return (
           <div className="d-flex flex-row justify-content-center align-items-center">
-            <p
-              className={
-                "text-center fs-2 font-weight-bold personName outline" +
-                outlineColor
-              }
-              style={{
-                color: person.color,
-              }}
-            >
-              <b>{person.username}</b>{" "}
+            <div className="d-flex flex-column justify-content-center">
               {person.mod ? (
                 <span
                   className="badge shadow"
@@ -68,10 +42,18 @@ function App() {
                   MOD
                 </span>
               ) : (
-                ""
+                <></>
               )}
+              <p
+                className={"fs-2 font-weight-bold personName"}
+                style={{
+                  color: person.color,
+                }}
+              >
+                <b>{person.username}</b>
+              </p>
               <p className="text-center text-white fs-3">{person.ign}</p>
-            </p>
+            </div>
           </div>
         );
       })}
