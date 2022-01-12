941283d8d05a899b44350807ab735fd9293e6d7d
JanuZz <jlp02.dk@gmail.com>
2022-01-12 18:04:18 +0100

    Update src

--- src/App.js
+++ src/App.js
@@ -1,69 +1,80 @@
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
+import { io } from "socket.io-client";
+import { useSearchParams } from "react-router-dom";
+
 function App() {
-  const [people, setPeople] = useState([]);
-  const [connected, setConnected] = useState(false);
+	const [people, setPeople] = useState([]);
+	const [connected, setConnected] = useState(false);
 
-  useEffect(() => {
-    console.log("Connecting to server...");
-    const socket = io("https://shadow-generated-jodhpur.glitch.me");
-    socket.on("connect", () => {
-      console.log("Connected to " + socket.id);
-      setConnected(true);
-    });
-    socket.on("queue", (list) => {
-      setPeople(list);
-      console.log(list);
-    });
+	const [searchParams] = useSearchParams();
 
-    return () => {
-      socket.removeAllListeners("queue");
-    };
-  }, []);
+	const c = searchParams.get("c");
 
-  return (
-    <>
-      <div className="rounded p-2 fw-bolder lh-base shadow m-2" style={{backgroundColor:"black"}}>
-        <p className="text-center fs-3 text-white mb-0">
-          {connected ? "Queue" : "Connecting..."}
-        </p>
-        <p className="text-center fs-6 text-muted font-weight-light mb-0">
-          Total: {people.length.toFixed(0)}
-        </p>
-      </div>
-      <div className="d-flex flex-column justify-content-start align-items-center">
-        {people.map((person, i) => {
-          return (
-            <div className="d-flex flex-row align-items-center">
-              {person.mod ? (
-                <span
-                  className="badge shadow"
-                  style={{ backgroundColor: "#ff0000" }}
-                >
-                  MOD
-                </span>
-              ) : (
-                <></>
-              )}
-              <p
-                className="fs-2 font-weight-bold px-3 textShadow"
-                style={{
-                  color: person.color ? person.color : "#ffffff",
-                }}
-              >
-                <b>{person.username}</b>
-              </p>
-              <p className="text-center ont-weight-light text-white fs-3 textShadow">
-                {person.ign}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+	useEffect(() => {
+		console.log("Connecting to server...");
+		const socket = io("https://shadow-generated-jodhpur.glitch.me");
+		socket.on("connect", () => {
+			console.log("Connected to " + socket.id);
+			setConnected(true);
+			socket.emit("#" + c);
+		});
+		socket.on("#" + c, (list) => {
+			setPeople(list);
+			console.log(list);
+		});
+
+		return () => {
+			socket.removeAllListeners("queue");
+		};
+	}, []);
+
+	return (
+		<>
+			<div
+				className="rounded p-2 fw-bolder lh-base shadow m-2"
+				style={{ backgroundColor: "black" }}
+			>
+				<p className="text-center fs-3 text-white mb-0">
+					{connected ? "Queue" : "Connecting..."}
+				</p>
+				<p className="text-center fs-6 text-muted font-weight-light mb-0">
+					Total: {people.length.toFixed(0)}
+				</p>
+			</div>
+			<div className="d-flex flex-column justify-content-start align-items-center">
+				{people.map((person, i) => {
+					return (
+						<div className="d-flex flex-row align-items-center">
+							{person.mod ? (
+								<span
+									className="badge shadow"
+									style={{ backgroundColor: "#ff0000" }}
+								>
+									MOD
+								</span>
+							) : (
+								<></>
+							)}
+							<p
+								className="fs-2 font-weight-bold px-3 textShadow"
+								style={{
+									color: person.color ? person.color : "#ffffff",
+								}}
+							>
+								<b>{person.username}</b>
+							</p>
+							<p className="text-center ont-weight-light text-white fs-3 textShadow">
+								{person.ign}
+							</p>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 reportWebVitals();
